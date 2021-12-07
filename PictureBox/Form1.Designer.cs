cbf60fd6eafae9ab1ff8cc76128f35b6218cb2a4
PhanNhutTruong <tuong22223@gmail.com>
2021-12-07 14:30:20 +0700

    Add project files.

---
+++ PictureBox/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(142, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(518, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(178, 158);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 38);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(371, 158);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 38);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(542, 158);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 38);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(371, 63);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(100, 35);
+            this.txtId.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(162, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(542, 63);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 35);
+            this.txtName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ PictureBox/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        RestoreEntities db = new RestoreEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Clear()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(pictureBox1.Image)) == false)
+            {
+                pictureBox1.Image.Dispose();
+            }
+
+            pictureBox1.Image = null;
+            pictureBox1.Name = null;
+            txtId.Text = null;
+            txtName.Text = null;
+        }
+
+        public void FillData()
+        {
+            var list = db.PictureTests.ToList();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = list;
+        }
+
+        string UrlofImage;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files() | *.jpg; *.jpeg; *.bmp; ";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(pictureBox1.Image)) == false)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+                pictureBox1.Image = null;
+                pictureBox1.Name = null;
+                pictureBox1.Image = new Bitmap(open.FileName);
+                pictureBox1.Name = Path.GetFileName(open.FileName);
+                UrlofImage = open.FileName;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            PictureTest picture = new PictureTest();
+            picture.name = txtName.Text;
+            if (string.IsNullOrEmpty(Convert.ToString(pictureBox1.Name)) == false)
+            {
+                var list = db.PictureTests.Where(c => c.imagename == pictureBox1.Name).ToList();
+                if (list.Count <= 0)
+                {
+                    File.Copy(UrlofImage, Path.Combine(Application.StartupPath + "\\picture\\", Convert.ToString(pictureBox1.Name)), true);
+                    picture.imagename = pictureBox1.Name;
+                }
+            }
+            db.PictureTests.Add(picture);
+            db.SaveChanges();
+            FillData();
+            Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            PictureTest picture = db.PictureTests.Where(c => c.id == id).First();
+            string name = System.IO.Path.Combine(Application.StartupPath + "\\picture\\", picture.imagename);
+            if (File.Exists(name))
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(pictureBox1.Image)) == false)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+                pictureBox1.Image = null;
+                var list = db.PictureTests.Where(c => c.imagename == picture.imagename).ToList();
+                if (list.Count >= 2)
+                {
+
+                }
+                else
+                {
+                    File.Delete(name);
+                }
+            }
+            db.PictureTests.Remove(picture);
+            db.SaveChanges();
+            FillData();
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            PictureTest picture = db.PictureTests.Where(c => c.id == id).First();
+            picture.name = txtName.Text;
+            if (string.IsNullOrEmpty(Convert.ToString(pictureBox1.Name)) == false)
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(picture.imagename)) == false)
+                {
+                    string name = System.IO.Path.Combine(Application.StartupPath + "\\picture\\", picture.imagename);
+                    if (File.Exists(name))
+                    {
+                        var list = db.PictureTests.Where(c => c.imagename == picture.imagename).ToList();
+                        if (list.Count >=2)
+                        {
+                            
+                        }
+                        else
+                        {
+                            File.Delete(name);
+                        }
+                    }
+                }
+                File.Copy(UrlofImage, Path.Combine(Application.StartupPath + "\\picture\\", Convert.ToString(pictureBox1.Name)), true);
+                picture.imagename = pictureBox1.Name;
+            }
+            db.SaveChanges();
+            FillData();
+            Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Clear();
+            int id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            PictureTest picture = db.PictureTests.Where(c => c.id == id).First();
+            if (string.IsNullOrEmpty(Convert.ToString(picture.imagename)))
+            {
+                pictureBox1.Image = null;
+            }
+            else
+            {
+                if (File.Exists(Application.StartupPath + "\\picture\\" + picture.imagename))
+                {
+                    pictureBox1.Image = new Bitmap(Application.StartupPath + "\\picture\\" + picture.imagename);
+                }
+            }
+            txtId.Text = picture.id.ToString();
+            txtName.Text = picture.name;
+        }
+    }
+}
